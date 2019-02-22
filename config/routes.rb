a2985926c60d2c534fda528f5f6174a33a5cbfc9
hironobu_doda <ironnamono.rakusaku@gmail.com>
2019-02-22 17:38:31 +0900

    メーラーの修正をした2

--- config/routes.rb
+++ config/routes.rb
@@ -11,4 +11,6 @@ Rails.application.routes.draw do
 
   mount LetterOpenerWeb::Engine, at: "/letter_opener" if Rails.env.development?
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
+
+  mount LetterOpenerWeb::Engine, at: "/letter_opener" if Rails.env.production?
 end
